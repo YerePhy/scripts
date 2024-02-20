8f3cc85a4ce17cb7886e79a99e8ac544b0d957a9
yere-qubio <jere@qubiotech.com>
2024-02-20 17:55:44 +0100

    Improving local mri subjects script.

--- rsync_local_mri_subjects.sh
+++ rsync_local_mri_subjects.sh
@@ -6,3 +6,5 @@ MRI_FILE_NAME="resampled_T1.nii"
 
 
 find "$SIMULATIONS_DATASET" -name "$MRI_FILE_NAME" -type f -exec dirname "{}" \; | xargs -I{} cp -rf {} "$ONLY_MRI_SUBJECTS" 
+mkdir -p "$ONLY_MRI_SUBJECTS"/51
+find "$ONLY_MRI_SUBJECTS" -maxdepth 1 -type f -exec mv {} "$ONLY_MRI_SUBJECTS"/51 \;
